535d32e4db016d39dbaea98f67ef1ffab4e46e2b
evepoi <55119879+evepoi@users.noreply.github.com>
2025-10-21 21:04:19 +0900

    2025.10.21 / 링크값 수정

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -9,12 +9,12 @@
       <div class="social-section">
         <p class="social-heading">social</p>
         <p class="social-link-item">
-          <a href="https://www.youtube.com/@YOUNGSUNGALLERY" target="_blank" rel="noopener noreferrer">
+          <a href="https://www.youtube.com/@영선갤러리" target="_blank" rel="noopener noreferrer">
             YouTube: 영선갤러리 유튜브에서 많은 영상을 볼 수 있습니다.
           </a>
         </p>
         <p class="social-link-item">
-          <a href="http://pf.kakao.com/_qxiBfxd" target="_blank" rel="noopener noreferrer">
+          <a href="https://open.kakao.com/o/gNPhwidf" target="_blank" rel="noopener noreferrer">
             카카오톡: 영선갤러리 카카오톡에서 다양한 정보를 얻으실 수 있습니다.
           </a>
         </p>
