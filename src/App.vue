73b56b46362df8d868471da72060a216dc4aa7db
evepoi <55119879+evepoi@users.noreply.github.com>
2025-12-09 12:55:44 +0900

    2025.12.09 / App.vue 내용 수정

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,8 @@
         <img src="/vite.svg" class="logo vite-logo" alt="Vite logo" />
       </a>
       <a href="https://vuejs.org/" target="_blank" rel="noopener noreferrer">
-        <img src="./assets/vue.svg" class="logo vue-logo" alt="Vue logo" />
+        <!-- './assets/vue.svg' -> '/vue.svg' 로 수정! -->
+        <img src="/vue.svg" class="logo vue-logo" alt="Vue logo" /> 
       </a>
       <span>Powered by Vite & Vue</span>
     </footer>
@@ -19,5 +20,7 @@
 <script setup>
 // 향후 전역 스타일 및 기타 스크립트 로직 추가
 // import './style.css'; // 이 파일은 나중에 정리할 예정
-import '@/styles/TheFooter.scss'; // 푸터 스타일 임포트 (아래에서 만들 예정)
+import '@/styles/TheFooter.scss'; // 푸터 스타일 임포트
+
+// 컴포넌트 스크립트 로직 추가
 </script>
