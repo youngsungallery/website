b735a14e7fb4dd454410904623d60c9875f2b75d
evepoi <55119879+evepoi@users.noreply.github.com>
2025-12-09 12:53:43 +0900

    2025.12.09 / 커밋 오류 수정

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import path from 'path'; // path 모듈 임포트
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/website/', // GitHub Pages 프로젝트 페이지 경로 설정
   plugins: [vue()],
-})
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'), // @ 별칭을 src 폴더로 설정
+    },
+  },
+});
