fcb0df934e2c52cf320f010a77657e279fa9d826
evepoi <55119879+evepoi@users.noreply.github.com>
2025-12-09 13:54:38 +0900

    2025.12.09 / 유튜브, 카카오톡 아이콘 수정

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -32,12 +32,12 @@
     <div class="footer-section social-links">
       <a href="https://www.youtube.com/@%EC%98%81%EC%84%A0%EA%B0%A4%EB%9F%AC%EB%A6%AC" target="_blank" rel="noopener noreferrer">
         <!-- 유튜브 아이콘 적용 -->
-        <img src="/youtube.svg" class="social-icon" alt="YouTube icon" /> 영선갤러리 유튜브
+        <img src="/youtube.png" class="social-icon" alt="YouTube icon" /> 영선갤러리 유튜브
       </a>
       <span> | </span>
       <a href="https://open.kakao.com/o/gNPhwidf" target="_blank" rel="noopener noreferrer">
         <!-- 카카오톡 아이콘 적용 -->
-        <img src="/kakaotalk.svg" class="social-icon" alt="KakaoTalk icon" /> 영선갤러리 카카오톡
+        <img src="/kakaotalk.png" class="social-icon" alt="KakaoTalk icon" /> 영선갤러리 카카오톡
       </a>
     </div>
   </footer>
