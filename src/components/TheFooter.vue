ade8b7812c7713e78099ada83b1f6513d60e5968
evepoi <55119879+evepoi@users.noreply.github.com>
2025-12-09 13:27:57 +0900

    2025.12.09 / 푸터 내용 변경

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -6,23 +6,37 @@
     <hr class="footer-divider"> <!-- 구분선 -->
     <div class="footer-section tech-stack">
       <a href="https://vitejs.dev" target="_blank" rel="noopener noreferrer">
-        <img src="/vite.svg" class="logo vite-logo" alt="Vite logo" />
+        <img src="/vite.svg" class="logo tech-logo" alt="Vite logo" />
       </a>
       <a href="https://vuejs.org/" target="_blank" rel="noopener noreferrer">
-        <img src="/vue.svg" class="logo vue-logo" alt="Vue logo" />
+        <img src="/vue.svg" class="logo tech-logo" alt="Vue logo" />
       </a>
       <span>Powered by Vite & Vue</span>
     </div>
     <hr class="footer-divider"> <!-- 구분선 -->
     <div class="footer-section contact-info">
-      <p>영선갤러리 | 경기 수원시 영통구 덕영대로 1471번길 2층</p>
-      <p>031-203-1089</p>
+      <p>
+        <a href="https://map.naver.com/p/search/%EC%98%81%EC%84%A0%EA%B0%A4%EB%9F%AC%EB%A6%AC" target="_blank" rel="noopener noreferrer">
+          경기 수원시 영통구 덕영대로 1471번길 2층
+          <span class="icon">🗺️</span> <!-- 지도 아이콘 -->
+        </a>
+      </p>
+      <p>
+        <a href="tel:031-203-1089">
+          031-203-1089
+          <span class="icon">📞</span> <!-- 전화 아이콘 -->
+        </a>
+      </p>
     </div>
     <hr class="footer-divider"> <!-- 구분선 -->
     <div class="footer-section social-links">
-      <a href="https://www.youtube.com/@youngsungallery" target="_blank" rel="noopener noreferrer">영선갤러리 유튜브</a>
+      <a href="https://www.youtube.com/@%EC%98%81%EC%84%A0%EA%B0%A4%EB%9F%AC%EB%A6%AC" target="_blank" rel="noopener noreferrer">
+        <span class="icon">▶️</span> 영선갤러리 유튜브
+      </a>
       <span> | </span>
-      <a href="https://pf.kakao.com/_xeTxoG" target="_blank" rel="noopener noreferrer">영선갤러리 카카오톡</a>
+      <a href="https://open.kakao.com/o/gNPhwidf" target="_blank" rel="noopener noreferrer">
+        <span class="icon">💬</span> 영선갤러리 카카오톡
+      </a>
     </div>
   </footer>
 </template>
