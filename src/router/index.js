b90742f3489aceacc42166d68879be6e4ef5a1e3
evepoi <evepoi@naver.com>
2026-01-21 12:18:56 +0900

    빌드 페이지 로드 오류 수정.

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,9 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../pages/Home.vue'
 
 export default createRouter({
-  history: createWebHistory(),
+  // ✅ Vite의 base(/ 또는 /website/)를 그대로 Router에 전달
+  history: createWebHistory(import.meta.env.BASE_URL),
+
   routes: [
     { path: '/', name: 'Home', component: Home },
   ],
